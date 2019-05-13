e50bd0b8d5f87d880f808934b06e378abc2ee343
BColwell131 <bcolwell@tcd.ie>
2019-05-13 14:14:53 +0100

    Setup eslint&prettier

--- scripts/calculateBearings.js
+++ scripts/calculateBearings.js
@@ -1,10 +1,11 @@
-//Imports
+// Imports
 const fs = require('fs');
 const path = require('path');
 
-//Globals
-let marks = JSON.parse(fs.readFileSync(path.join(__dirname, '../', 'data', "marks.json")));
-let bearing = 0;
+// Globals
+const marks = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '../', 'data', 'marks.json'))
+);
 
 // Converts from radians to degrees.
 Math.degrees = function(radians) {
@@ -14,30 +15,40 @@ Math.degrees = function(radians) {
 // Converts from degrees to radians.
 Math.radians = function(degrees) {
   return degrees * (Math.PI / 180);
-}
-
+};
 
 // Convert to decimal degrees (DD) from degrees and decimal minutes (DDM)
 let latDD;
-for(markname in marks){
-    let mark = marks[markname];
-    latDD   = mark.lattitude.deg + (mark.lattitude.min/60);
-    longDD  = mark.longitude.deg + (mark.longitude.min/60);
-    marks[markname].latDD = latDD;
-    marks[markname].longDD = -longDD; //TODO: this negative indicates W
-};
+let longDD;
+for (const markname in marks) {
+  const mark = marks[markname];
+  latDD = mark.lattitude.deg + mark.lattitude.min / 60;
+  longDD = mark.longitude.deg + mark.longitude.min / 60;
+  marks[markname].latDD = latDD;
+  marks[markname].longDD = -longDD; // TODO: this negative indicates W
+}
 
-for(originMarkname in marks){
-  let originMark = marks[originMarkname];
-  for(destMarkname in marks){
-    if(originMarkname != destMarkname){
-      let destMark = marks[destMarkname];
-      //Calculate bearing
-      const y = Math.sin(Math.radians(destMark.longDD)-Math.radians(originMark.longDD)) * Math.cos(Math.radians(destMark.latDD));
-      const x = Math.cos(Math.radians(originMark.latDD))*Math.sin(Math.radians(destMark.latDD)) - Math.sin(Math.radians(originMark.latDD))*Math.cos(Math.radians(destMark.latDD))*Math.cos(Math.radians(destMark.longDD)-Math.radians(originMark.longDD));
+for (const originMarkname in marks) {
+  const originMark = marks[originMarkname];
+  for (const destMarkname in marks) {
+    if (originMarkname !== destMarkname) {
+      const destMark = marks[destMarkname];
+      // Calculate bearing
+      const y =
+        Math.sin(
+          Math.radians(destMark.longDD) - Math.radians(originMark.longDD)
+        ) * Math.cos(Math.radians(destMark.latDD));
+      const x =
+        Math.cos(Math.radians(originMark.latDD)) *
+          Math.sin(Math.radians(destMark.latDD)) -
+        Math.sin(Math.radians(originMark.latDD)) *
+          Math.cos(Math.radians(destMark.latDD)) *
+          Math.cos(
+            Math.radians(destMark.longDD) - Math.radians(originMark.longDD)
+          );
       let brng = Math.degrees(Math.atan2(y, x));
-      brng = (brng+360)%360; //
-      brng += 5; //Account for 5deg west variation of magnetic North.
+      brng = (brng + 360) % 360; //
+      brng += 5; // Account for 5deg west variation of magnetic North.
       brng = Math.round(brng);
       marks[originMarkname].bearings[destMarkname] = brng;
     }
